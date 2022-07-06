185f96c8e1f332aa982e8cf7d700628088ce1618
a1eeexey <a1exeeey@mail.ru>
2022-07-06 15:55:03 +0300

    first commit

---
+++ cooking_vue/src/components/CategoryBox.vue
@@ -0,0 +1,39 @@
+<template>
+    <!-- Здесь кнопки с категориями, спавнятся только первые две (надо сделать больше но сколько - это от размера виджета зависит)-->
+    <li v-for="category in Categories.slice(0, 2)" :key="category.id" :category="category">
+        <div class="column is-3">
+            <div class="box">
+                <button v-on:click="SetCategory(category.get_absolute_url)" class="is-size-12">{{ category.name }}</button>
+            </div>
+        </div>
+        <!-- Реализация выпадающего списка на вьютифай. Вьютифая у нас кста нет и не будет. Короче тут надо по-хорошему
+        сделать выпадающий список со всеми категориями.-->
+        <v-select
+            :items="Categories"
+            v-on:change="SetCategory(category.get_absolute_url)"
+            label="..."
+            multiple
+            hint="Больше категорий"
+            persistent-hint
+        ></v-select>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'CategoryBox',
+    props: {
+        Categories: Array,
+        category: Object,
+        SetCategory: Function
+    }
+}
+</script>
+
+<style scoped>
+  .image {
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+</style>
